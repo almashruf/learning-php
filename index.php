a957df48c475c399b6fec87e7e2943dcee30ef78
almashruf <tonoy5300@gmail.com>
2025-04-13 21:22:35 +0600

    intro

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="http://0.0.0.0:5000/public/about.php">
+</head>
+<body>
+    echo "hello world"
+    <?php
+    $txt = "tonoy";
+    echo "my name is $txt!"
+    ?>
+
+<!-- <script src=""></script> -->
+</body>
+</html>
---
+++ public/about.php
@@ -0,0 +1,14 @@
+<?php
+
+// Replace content type text/html to applciaton javascript
+header('Content-type: text/css; charset=UTF-8');
+
+echo "
+
+  body {
+
+     background: gray;
+  }
+
+";
+
